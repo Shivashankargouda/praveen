885fd575e77041ca470d905e44a8a8bcd86c660e
Shivashankargouda <112755225+Shivashankargouda@users.noreply.github.com>
2023-05-30 12:10:58 +0530

    Add files via upload

---
+++ Exp8.java
@@ -0,0 +1,16 @@
+class Exp8
+{
+	public static void main(String s[])
+	{
+		int n=5;
+	if (n%2==0)
+	{
+		System.out.println("even");
+	}
+	else
+	{
+		System.out.println("odd");
+	}
+		System.out.println("welcome");
+	}
+}
